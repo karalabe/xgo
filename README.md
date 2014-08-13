d2febd9ebae409743a703256c8fe28614005d54e
Péter Szilágyi <peterke@gmail.com>
2014-08-13 15:09:53 +0300

    Update xgo to versioned containers.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-xgo - Go CGO cross compiler
-===========================
+# xgo - Go CGO cross compiler
 
 Although Go strives to be a cross platform language, cross compilation from one
 platform to another is not as simple as it could be, as you need the Go sources
@@ -26,8 +25,7 @@ libraries. This becomes very annoying when you need access only to some trivial
 OS specific functionality (e.g. query the CPU load), but need to configure and
 maintain separate build environments to do it.
 
-Enter xgo
----------
+## Enter xgo
 
 My solution to the challenge of cross compiling Go code with embedded C snippets
 (i.e. CGO_ENABLED=1) is based on the concept of [lightweight Linux containers](http://en.wikipedia.org/wiki/LXC).
@@ -35,21 +33,19 @@ All the necessary Go tool-chains, C cross compilers and platform headers/librari
 have been assembled into a single Docker container, which can then be called as if
 a single command to compile a Go package to various platforms and architectures.
 
-Installation
-------------
+## Installation
 
 Although you could build the container manually, it is available as an automatic
-trusted build from Docker's container registry (67MB base + 472MB xgo):
+trusted build from Docker's container registry (~530MB):
 
-    docker pull karalabe/xgo
+    docker pull karalabe/xgo-latest
 
 To prevent having to remember a potentially complex Docker command every time,
 a lightweight Go wrapper was written on top of it.
 
     go get github.com/karalabe/xgo
 
-Usage
------
+## Usage
 
 Simply specify the import path you want to build, and xgo will do the rest:
 
@@ -64,3 +60,25 @@ Simply specify the import path you want to build, and xgo will do the rest:
     -rwxr-xr-x  1 root     root  4151688 Aug  7 10:01 iris-linux-arm
     -rwxr-xr-x  1 root     root  4228608 Aug  7 10:01 iris-windows-386.exe
     -rwxr-xr-x  1 root     root  5243904 Aug  7 10:01 iris-windows-amd64.exe
+
+### Go releases
+
+As newer versions of the language runtime, libraries and tools get released,
+these will get incorporated into xgo too as extensions layers to the base cross
+compilation image (only Go 1.3 and above will be supported).
+
+You can select which Go release to work with through the `-go` command line flag
+to xgo and if the specific release was already integrated, it will automatically
+be retrieved and installed.
+
+    $ xgo -go 1.3.0 github.com/project-iris/iris
+    ...
+
+Since xgo depends on not only the official releases, but also on Dave Cheney's
+ARM packages, there will be a slight delay between official Go updates and the
+xgo updates.
+
+Additionally, a few wildcard release strings are also supported:
+
+  - `-go latest` will use the latest Go release
+  - `-go 1.3.x` will use the latest point release of a specific Go version
--- xgo.go
+++ xgo.go
@@ -7,6 +7,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -14,31 +16,81 @@ import (
 	"os/exec"
 )
 
-// Docker container configured for cross compilation.
-var container = "karalabe/xgo"
+// Cross compilation docker containers
+var dockerBase = "karalabe/xgo-base"
+var dockerDist = "karalabe/xgo-"
+
+// Command line arguments to fine tune the compilation
+var goVersion = flag.String("go", "latest", "Go release to use for cross compilation")
 
 func main() {
-	// Make sure docker is actually available on the system
-	fmt.Println("Checking docker installation...")
-	if err := run(exec.Command("docker", "version")); err != nil {
+	flag.Parse()
+
+	// Ensure docker is available
+	if err := checkDocker(); err != nil {
 		log.Fatalf("Failed to check docker installation: %v.", err)
 	}
-	fmt.Println()
-
-	// Fetch and configure the compilation settings
-	if len(os.Args) != 2 {
-		log.Fatalf("Usage: %s <go import path>", os.Args[0])
+	// Validate the command line arguments
+	if len(flag.Args()) != 1 {
+		log.Fatalf("Usage: %s [options] <go import path>", os.Args[0])
 	}
-	path := os.Args[1]
-	pwd, err := os.Getwd()
+	// Check that all required images are available
+	found, err := checkDockerImage(dockerDist + *goVersion)
+	switch {
+	case err != nil:
+		log.Fatalf("Failed to check docker image availability: %v.", err)
+	case !found:
+		fmt.Println("not found!")
+		if err := pullDockerImage(dockerDist + *goVersion); err != nil {
+			log.Fatalf("Failed to pull docker image from the registry: %v.", err)
+		}
+	default:
+		fmt.Println("found.")
+	}
+	// Cross compile the requested package into the local folder
+	if err := compile(flag.Args()[0]); err != nil {
+		log.Fatalf("Failed to cross compile package: %v.", err)
+	}
+}
+
+// Checks whether a docker installation can be found and is functional.
+func checkDocker() error {
+	fmt.Println("Checking docker installation...")
+	if err := run(exec.Command("docker", "version")); err != nil {
+		return err
+	}
+	fmt.Println()
+	return nil
+}
+
+// Checks whether a required docker image is available locally.
+func checkDockerImage(image string) (bool, error) {
+	fmt.Printf("Checking for required docker image %s... ", image)
+	out, err := exec.Command("docker", "images", "--no-trunc").Output()
+	if err != nil {
+		return false, err
+	}
+	return bytes.Contains(out, []byte(image)), nil
+}
+
+// Pulls an image from the docker registry.
+func pullDockerImage(image string) error {
+	fmt.Printf("Pulling %s from docker registry...\n", image)
+	fmt.Printf("Note, this may take some time, but due to a docker bug, progress cannot be displayed.\n")
+	return run(exec.Command("docker", "pull", image))
+}
+
+// Cross compiles a requested package into the current working directory.
+func compile(path string) error {
+	folder, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("Failed to retrieve the working directory: %v.", err)
 	}
-	// Cross compile the requested package into the local folder
 	fmt.Printf("Cross compiling %s...\n", path)
-	if err := run(exec.Command("docker", "run", "-v", pwd+":/build", container, path)); err != nil {
-		log.Fatalf("Failed to cross compile package: %v.", err)
+	if err := run(exec.Command("docker", "run", "-v", folder+":/build", dockerDist+*goVersion, path)); err != nil {
+		return err
 	}
+	return nil
 }
 
 // Executes a command synchronously, redirecting its output to stdout.
