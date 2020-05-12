a7834c36fa1f2cd31948f23a7024286551609011
anjmao <andzej.maciusovic@gmail.com>
2020-05-12 16:50:10 +0300

    Update test example

--- testlib/mobile.go
+++ testlib/mobile.go
@@ -1,13 +1,5 @@
 package testlib
 
-import (
-	"fmt"
-
-	"github.com/mysteriumnetwork/go-openvpn/openvpn3"
-)
-
 func HelloWorld() string {
-	config := openvpn3.NewConfig("test")
-	fmt.Println(config)
 	return "Hello"
 }
